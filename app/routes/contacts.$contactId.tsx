e829e13a49227db1bb49496f27b271a0d51e1a14
Sherajul-Shakil <sherajulshakil14@cse.pstu.ac.bd>
2023-11-28 09:36:12 +0600

    Form submit without navigation

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,10 +1,10 @@
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData, useFetcher, } from "@remix-run/react";
 import type { FunctionComponent } from "react";
-import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { json, LoaderFunctionArgs, ActionFunctionArgs, } from "@remix-run/node";
 import invariant from "tiny-invariant";
 
 import type { ContactRecord } from "../data";
-import { getContact } from "../data";
+import { getContact, updateContact } from "../data";
 
 
 // Invariant is a handy function for throwing an error with a custom message 
@@ -20,6 +20,7 @@ export const loader = async ({
     return json({ contact });
 };
 
+// get contact with id
 export default function Contact() {
 
     const { contact } = useLoaderData<typeof loader>();
@@ -83,13 +84,29 @@ export default function Contact() {
     );
 }
 
+// update contact with id
+export const action = async ({
+    params,
+    request,
+}: ActionFunctionArgs) => {
+    invariant(params.contactId, "Missing contactId param");
+    const formData = await request.formData();
+    return updateContact(params.contactId, {
+        favorite: formData.get("favorite") === "true",
+    });
+};
+
 const Favorite: FunctionComponent<{
     contact: Pick<ContactRecord, "favorite">;
 }> = ({ contact }) => {
-    const favorite = contact.favorite;
+    const fetcher = useFetcher();
+    // const favorite = contact.favorite;
+    const favorite = fetcher.formData
+        ? fetcher.formData.get("favorite") === "true"
+        : contact.favorite;
 
     return (
-        <Form method="post">
+        <fetcher.Form method="post">
             <button
                 aria-label={
                     favorite
@@ -101,6 +118,6 @@ const Favorite: FunctionComponent<{
             >
                 {favorite ? "★" : "☆"}
             </button>
-        </Form>
+        </fetcher.Form>
     );
 };
